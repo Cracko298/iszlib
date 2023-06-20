9d2fb4f5097c0d8ab663bf1abbcc3704fc3f106b
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2023-06-20 14:00:14 -0400

    Add files via upload

---
+++ _dat/MMplugins/plugin.py
@@ -0,0 +1,13 @@
+def Credits():
+    ## Credits for Creator + Version Number (Example v3.2.2)
+    global version, developer
+    version = "v1.0.0"
+    developer = "Example Creator"
+    return version, developer
+Credits()
+
+def MainApp():
+    ## Your Code Here.
+    print("This is an Example Plugin.")
+
+    
